e5189b04317e0336a184fc44e5e38ed4337a8972
kyunghee <rkh97@naver.com>
2024-08-12 17:48:42 +0900

    영섭이 바보

---
+++ module/optimization/optimizer.py
@@ -0,0 +1,6 @@
+from .sgd.sgd_optimizer import SGDOptimizer
+# from module.optimization.uq.uq_optimizer import UQOptimizer
+# from module.optimization.bo.bo_optimizer import BOOptimizer
+# from module.optimization.vi.vi_optimizer import VIOptimizer
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
